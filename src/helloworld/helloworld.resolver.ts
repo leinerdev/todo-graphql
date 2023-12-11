5ad7bce1d8092a208da070c670b4f0722946e11c
Leiner Jose Barrios Medina - Ceiba Software <leiner.barrios@ceiba.com.co>
2023-12-11 13:29:23 -0500

    Video 24

--- src/helloworld/helloworld.resolver.ts
+++ src/helloworld/helloworld.resolver.ts
@@ -1,4 +1,14 @@
-import { Resolver } from '@nestjs/graphql';
+import { Float, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
-export class HelloworldResolver {}
+export class HelloworldResolver {
+    @Query(() => String, { description: 'Hola mundo es lo que retorna', name: 'Hello' })
+    helloworld(): string {
+      return 'Hello World!';
+    }
+
+    @Query(() => Float, { name: 'randomNumber' })
+    randomNumber(): number {
+      return Math.random() * 100;
+    }
+}
